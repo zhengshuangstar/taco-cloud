e60325f0b4d06c13a0bc1959b3f5a35717f849b6
zhengshuang <zuluck2004@163.com>
2021-08-29 23:02:23 +0800

    feat: ch2 view controller completed

---
+++ src/main/java/com/example/tacocloud/data/IngredientRepository.java
@@ -0,0 +1,9 @@
+package com.example.tacocloud.data;
+
+import com.example.tacocloud.Ingredient;
+
+public interface IngredientRepository {
+    Iterable<Ingredient> findAll();
+    Ingredient findOne(String id);
+    Ingredient save(Ingredient ingredient);
+}
--- src/main/java/com/example/tacocloud/web/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/web/DesignTacoController.java
@@ -6,8 +6,13 @@ import com.example.tacocloud.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,6 +23,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/design")
 public class DesignTacoController {
 
+    @GetMapping
     public String showDesignForm(Model model){
         List<Ingredient> ingredients= Arrays.asList(
                 new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
@@ -41,11 +47,19 @@ public class DesignTacoController {
         return "design";
     }
 
+    @PostMapping
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors){
+        if (errors.hasErrors()){
+            return "design";
+        }
+        log.info("Processing design: "+design);
+        return "redirect:/orders/current";
+    }
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return  ingredients.stream().filter(x->x.getType().equals(type)).collect(Collectors.toList());
 
     };
 
-    ;
+
 
 }
